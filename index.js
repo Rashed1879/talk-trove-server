d9b66d086d43e5ce276959f0c6d97b4e07ae95d6
Rashed1879 <rashedul15-1879@diu.edu.bd>
2023-06-10 16:35:20 +0600

    Api created for getting all users

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ async function run() {
 		const userCollection = client.db('talkTroveDB').collection('users');
 
 		// user related Api
+		app.get('/users', async (req, res) => {
+			const result = await userCollection.find().toArray();
+			res.send(result);
+		});
+
 		app.post('/users', async (req, res) => {
 			const user = req.body;
 			const query = { email: user.email };
