fd53c395ace6bd2d5022899d5b82b798f71d2ed7
Rashed1879 <rashedul15-1879@diu.edu.bd>
2023-06-10 17:35:50 +0600

    Api created for make admin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 // mongo db
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obhsxav.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -43,6 +43,18 @@ async function run() {
 				return res.send({ message: 'user already exists' });
 			}
 			const result = await userCollection.insertOne(user);
+			res.send(result);
+		});
+
+		app.patch('/users/admin/:id', async (req, res) => {
+			const id = req.params.id;
+			const filter = { _id: new ObjectId(id) };
+			const updateDoc = {
+				$set: {
+					role: 'admin',
+				},
+			};
+			const result = await userCollection.updateOne(filter, updateDoc);
 			res.send(result);
 		});
 
